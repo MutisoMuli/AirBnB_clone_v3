77a2f3860525734ff7b2ae8275ee1765da37463b
anto-muli <memuli@students.uonbi.ac.ke>
2024-04-01 15:14:49 +0300

    Done

--- models/user.py
+++ models/user.py
@@ -28,10 +28,10 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         if kwargs:
-            pwd = kwargs.pop('password'), None)
+            pwd = kwargs.pop('password', None)
             if pwd:
                 hasher = hashlib.md5()
                 hasher.update(pwd.encode('utf-8'))
-                secure_pwd = hasher.hexdigest ()
+                secure_pwd = hasher.hexdigest()
                 kwargs['password'] = secure_pwd
         super().__init__(*args, **kwargs)
